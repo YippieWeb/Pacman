0746ca254c787ae4dc27f8ae482bce0c28c37525
jasyyyan <jasmine.yip1031@gmail.com>
2023-12-18 15:36:58 +1100

    Partially set up board with dots

---
+++ pacman.py
@@ -0,0 +1,38 @@
+from board import boards
+import pygame
+
+pygame.init()
+
+WIDTH = 900
+HEIGHT = 950
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
+level = boards
+
+def draw_board():
+    num1 = ((HEIGHT - 50) // 32)
+    num2 = (WIDTH // 30)
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            # map numbers to corresponding tile type
+            # small white dot
+            if level[i][j] == 1:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
+            # big white dot
+            if level[i][j] == 2:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('black')
+    draw_board()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
